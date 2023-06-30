9570315854e36370aeaa8a358b53500c9bddc346
Lucas Rafael <lucasrafaelinfo@gmail.com>
2023-06-30 20:49:56 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/lucasrafael/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lucasrafael.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lucasrafael.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByEmail(String email);
+}
